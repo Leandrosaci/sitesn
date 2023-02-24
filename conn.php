770c80abc6a657544d52199b34fc797d25bea2ee
LeandroDev <52272254+Leandrosaci@users.noreply.github.com>
2023-02-24 16:31:32 -0300

    up4

--- conn.php
+++ conn.php
@@ -1,12 +1,11 @@
 <?php
 
 
-$mysqli = new mysqli('containers-us-west-157.railway.app', 'root', 'LZwvOsVfHbvL93oboN9t','railway','6330');
-if (mysqli_connect_errno()) {
-	echo "Failed to connect to MySQL: ".mysqli_connect_error();
-	exit();
-}else{
-	echo "connected ";	
-}
-
+session_start();
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+$db=mysqli_connect("DB_HOST","DB_USER","DB_PASSWORD","DB_NAME","DB_PORT");
 ?> 
