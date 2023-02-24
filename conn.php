20a1ac884cafb084b6813494a1436dea711d962a
LeandroDev <52272254+Leandrosaci@users.noreply.github.com>
2023-02-24 16:46:28 -0300

    up5

--- conn.php
+++ conn.php
@@ -9,3 +9,24 @@ $DB_NAME = $_ENV["DB_NAME"];
 $DB_PORT = $_ENV["DB_PORT"];
 $db=mysqli_connect("DB_HOST","DB_USER","DB_PASSWORD","DB_NAME","DB_PORT");
 ?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle</title>
+
+    <style>
+        body{
+            background: linear-gradient(to right, rgb(147, 20, 220), rgb(54, 17, 71));
+            color: white;
+            
+        }
+      </style>
+</head>
+  <body>
+    <h1>ola mundo</h1>
+  </body>
+  </html>
