e3e3f8054339dbae129ea825ead4d783ddb076b3
LeandroDev <52272254+Leandrosaci@users.noreply.github.com>
2023-02-24 15:48:09 -0300

    update2

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$mysqli = new mysqli('railway', 'root', 'LZwvOsVfHbvL93oboN9t', 'containers-us-west-157.railway.app');
+$mysqli = new mysqli('railway:6330', 'root', 'LZwvOsVfHbvL93oboN9t', 'containers-us-west-157.railway.app');
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: ".mysqli_connect_error();
 	exit();
