d38f7d595a253acb304d7a9694b3a41b6ad38e7e
LeandroDev <52272254+Leandrosaci@users.noreply.github.com>
2023-02-24 15:46:58 -0300

    update conn

--- conn.php
+++ conn.php
@@ -1,11 +1,7 @@
 <?php
-$db_name = "railway";
-$mysql_username = "root";
-$mysql_password = "LZwvOsVfHbvL93oboN9t";
-$server_name = "containers-us-west-157.railway.app";
 
 
-$mysqli = new mysqli('localhost', 'root', '1234', 'oficina');
+$mysqli = new mysqli('railway', 'root', 'LZwvOsVfHbvL93oboN9t', 'containers-us-west-157.railway.app');
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: ".mysqli_connect_error();
 	exit();
